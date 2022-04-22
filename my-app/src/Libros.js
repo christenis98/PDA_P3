154f62e62d1b0da31b09c7fc07e24df4a6ffd006
christenis98 <canei01@ucm.es>
2022-04-22 19:27:07 +0200

    Conexion API sin acabar

--- my-app/src/Libros.js
+++ my-app/src/Libros.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Busqueda from './Busqueda';
-
+import request from 'superagent';
 
 
 class Libros extends Component{
@@ -12,6 +12,15 @@ class Libros extends Component{
             authorField: '',
             titleField: ''
         }
+    }
+
+    buscarLibro = () =>{
+        request
+        .get("https://www.googleapis.com/books/v1/volumes")
+        .query({q1:this.authorField, q2:this.titleField})
+        .then((data) =>{
+            
+        })
     }
 
     hacerBusquedaAutor = (e) => {
